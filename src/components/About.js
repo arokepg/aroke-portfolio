6a8f64ce6580cad9eebddb38f8eb1e3813493ab8
Lơ <noobthongminh2007@gmail.com>
2024-09-07 11:44:34 +0700

    Outsider fix

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import profilePic from '../assets/images/profile.jpg'; // Add your profile pictu
 
 const About = () => {
   return (
-    <section id="about" className="py-20 bg-gray-200 text-nord1 dark:bg-nord1 dark:text-nord4 ">
+    <section id="about" className="py-20 bg-gray-100 text-nord1 dark:bg-nord1 dark:text-nord4 ">
       <div className="container mx-auto flex flex-col md:flex-row items-center">
         <img src={profilePic} alt="Your Name" className="w-48 h-48 rounded-full border-4 border-nord0 dark:border-nord6 mx-auto md:mx-0 md:mr-16" />
         <div className="mt-8 md:mt-0">
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import { FaArrowDown } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <section id="hero" className="h-screen flex flex-col justify-center items-center bg-nord6 text-nord0 dark:bg-nord3 dark:text-nord4">
+    <section id="hero" className="h-screen flex flex-col justify-center items-center bg-gray-50 text-nord0 dark:bg-nord3 dark:text-nord4">
       <h2 className="text-center text-4xl md:text-6xl font-bold mb-4">Hello, I'm Nhat Minh</h2>
       <p className="text-xl md:text-2xl mb-8">I'm a student who have the enthusiasm in computer</p>
       <Link to="projects" smooth={true} duration={500} className="flex items-center text-nord10 dark:text-nord7 cursor-pointer0">
